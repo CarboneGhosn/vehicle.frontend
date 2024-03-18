f021e9138d7747ce42221f25a72532442485a663
CarboneGhosn <ghosn.dani@hotmail.com>
2024-03-18 15:18:57 -0300

    crud using spring boot and angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,21 +10,44 @@ import { VehicleInputComponent } from './vehicle-input/vehicle-input.component';
 import { VehicleDisplayComponent } from './vehicle-display/vehicle-display.component';
 import { VehicleEditComponent } from './vehicle-edit/vehicle-edit.component';
 import { VehicleWrapperComponent } from './vehicle-wrapper/vehicle-wrapper.component';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomeContentComponent } from './welcome-content/welcome-content.component';
+import { AxiosService } from './axios.service';
+import { AuthContentComponent } from './auth-content/auth-content.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ContentComponent } from './content/content.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { VehiclePageComponent } from './vehicle-page/vehicle-page.component';
+
+
+
 
 @NgModule({
+
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      BrowserAnimationsModule,
+      VehicleInputComponent,
+      VehicleDisplayComponent,
+      VehicleEditComponent,
+      VehicleWrapperComponent,
+      AppRoutingModule,
+      FormsModule
+
+
+  ],
   declarations: [
     AppComponent,
+    WelcomeContentComponent,
+    AuthContentComponent,
+    LoginFormComponent,
+    ContentComponent,
+    ButtonsComponent,
+    VehiclePageComponent,
+
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    VehicleInputComponent,
-    VehicleDisplayComponent,
-    VehicleEditComponent,
-    VehicleWrapperComponent,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AxiosService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
